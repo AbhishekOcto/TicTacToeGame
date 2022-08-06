aece6fb06b3219bfe4acce389fba003dfa512803
Abhishek <you@example.com>
2022-08-06 20:25:22 +0530

    Modified: [Abhishek] Added player's input symbol X or 0

--- src/com/bridgelabz/ticTacToe/TicTacToe.java
+++ src/com/bridgelabz/ticTacToe/TicTacToe.java
@@ -1,23 +1,43 @@
 package com.bridgelabz.ticTacToe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
-	
-	 private static char[] board = new char[10];
-	
-	 /***
+
+    private static char[] board = new char[10];
+
+    static private char player1;
+
+    static Scanner sc = new Scanner(System.in);
+
+    /***
      * UC-1 - This method is Assigning space to Board Array
      */
-	 
     static void startGame(){
         for( int i = 1; i <= 9; i++) {
             board[i] = ' ';
         }
     }
 
-	public static void main(String[] args) {
-		
-		System.out.println("***Welcome to Tic Tac Toe Simulation Program***");
-		startGame();
-	}
+    /**
+     * UC-2 - Taking Playing symbol from User as X OR O only
+     */
+    static void playerInput(){
+        System.out.println("Player 1 Please Select the Symbol (X/O) :");
+        player1 = sc.next().charAt(0);
+        if( player1 == 'X' || player1 == 'O') {
+            System.out.println("Player 1 Symbol : " +player1);
+        } else {
+            System.out.println("Invalid Symbol. Please Enter the X or O only.");
+            playerInput();
+        }
+    }
 
+    public static void main(String[] args) {
+        System.out.println("Welcome to Tic Tac Toe Simulation");
+
+        startGame();
+
+        playerInput();
+    }
 }
